9d1b39869f4428cf7b9564cb412460ca773cb11c
grayweling <jy6228@yahoo.com>
2022-06-06 16:37:51 -0500

    thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
 
+const { getAllThoughts, getThoughtById, createThought, updateThought, deleteThought, addReaction, removeReaction } = require('../controllers/thoughtController');
 
+router
+    .route('/')
+    .get(getAllThoughts)
 
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+
+router
+    .route('/:UserId')
+    .post(createThought)
+
+router
+    .route('/:userId/:thoughtId')
+    .put(updateThought)
+    .delete(deleteThought)
+    .put(addReaction)
+
+router
+    .route('/:userId/:thoughtId/:reactionId')
+    .delete(removeReaction)
 
 module.exports = router;
